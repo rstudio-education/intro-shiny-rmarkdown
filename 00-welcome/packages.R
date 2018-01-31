4e65b9d7467f46f1ec42b7d0442d01d73e296876
Mine Cetinkaya-Rundel <mine@stat.duke.edu>
2018-01-31 03:56:43 -0800

    A more complete list and load all

--- 00-welcome/packages.R
+++ 00-welcome/packages.R
@@ -1,7 +1,8 @@
 # Install packages that will be used in the workshop
 from_cran <- c("shiny", "rmarkdown", 
-               "DT", "devtools", "flexdashboard", "gapminder", 
-               "shinydashboard", "shinythemes", "tidyverse")
+               "DT", "devtools", "flexdashboard", "gapminder",
+               "rticles", "shinydashboard", "shinythemes", 
+               "tidyverse", "tufte", "xaringan")
 
 install.packages(from_cran, repos = "http://cran.rstudio.com")
 
@@ -9,4 +10,13 @@ install.packages(from_cran, repos = "http://cran.rstudio.com")
 
 library(shiny)
 library(rmarkdown)
-...
+library(DT)
+library(devtools)
+library(flexdashboard)
+library(gapminder)
+library(rticles)
+library(shinydashboard)
+library(shinythemes)
+library(tidyverse)
+library(tufte)
+library(xaringan)
