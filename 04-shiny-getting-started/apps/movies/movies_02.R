5dd132881d362277b4cb6ac0164b5a5c2798ae5a
Mine Cetinkaya-Rundel <mine@stat.duke.edu>
2018-02-01 10:43:16 -0800

    back to answer

--- 04-shiny-getting-started/apps/movies/movies_02.R
+++ 04-shiny-getting-started/apps/movies/movies_02.R
@@ -42,7 +42,7 @@ server <- function(input, output) {
   
   # Create scatterplot object the plotOutput function is expecting --
   output$scatterplot <- renderPlot({
-    ggplot(data = movies, aes_string(x = input$x, y = input$y,
+    ggplot(data = movies, aes_string(x = input$x, y = input$y, 
                                      color = input$z)) +
       geom_point()
   })
